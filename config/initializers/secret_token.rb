8b48ce96f814be68f570327c3b4a1c5f305544f9
Justin Fedick <justin.fedick@gmail.com>
2010-12-03 15:30:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caffeinetracker::Application.config.secret_token = 'ae774c20f0b7873a7593f3e757212079168e7e486a7138fbc47fccf7bf6e7b4e90f9b15a559443a883a071495b9bdf38ce8d4d7fb9f5a399874f12229ba38b18'
