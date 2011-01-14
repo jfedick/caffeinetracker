285fde3122c6a02d8ab82a4f09fe062dd3fefc01
Justin Fedick <justin.fedick@gmail.com>
2011-01-14 15:33:16 -0500

    Add create and new methods to the cups controller.

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -1,7 +1,21 @@
 class CupsController < ApplicationController
-
   def new
-    @Cups = Cups.new
+    @cups = Cups.new
+  end
+  
+  def create
+    @cups = Cups.new(params[:cups])
+    if @cups.save
+      flash[:success] = "Wow"
+      redirect_to cups_path
+    else
+      flash.now[:error] = "Please try again"
+      render :action => :new
+    end
+  end
+  
+  def index
+    @cups = Cups.all
   end
 
 end
